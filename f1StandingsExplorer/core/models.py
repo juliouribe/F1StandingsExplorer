8fee3dd8955ceacc826eae34117ea439199e02fc
Julio C. Uribe <uribejulioc@gmail.com>
2023-03-28 12:12:11 -0700

    Add Constructors model and create the 10 teams in db

--- f1StandingsExplorer/core/models.py
+++ f1StandingsExplorer/core/models.py
@@ -1,2 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 
+
+class Constructor(models.Model):
+    name = models.CharField(max_length=255)
+    founded = models.DecimalField(max_digits=4, decimal_places=0)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
