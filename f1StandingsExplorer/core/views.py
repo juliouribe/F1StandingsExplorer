9f418e7156818e4b11b7ef1a5d0f9fe9810cc6fc
Julio C. Uribe <uribejulioc@gmail.com>
2023-04-14 08:54:32 -0700

    Create and display points table in the home page

--- f1StandingsExplorer/core/views.py
+++ f1StandingsExplorer/core/views.py
@@ -6,30 +6,36 @@ from driver.models import Driver
 
 
 def home(request):
-    headers = list(GrandPrix.objects.filter(
+    headers = ['Driver'] + list(GrandPrix.objects.filter(
         race_date__year=2022).values_list('race_track__country_abbreviation', flat=True))
+    headers.append('Points')
     # .values() essentially does the group by.
     results = RaceResult.objects.filter(
         grand_prix__race_date__year=2022).values('driver').annotate(
         total_points=Sum('points')).order_by('-total_points')
 
-    print("Results: " + str(len(results)))
-    for result in results:
-        print(result)
+    drivers = Driver.objects.filter(raceresults__grand_prix__race_date__year=2022).annotate(
+        total_points=Sum('raceresults__points')).values('id', 'first_name', 'last_name', 'total_points').order_by(
+            '-total_points')
 
-    # Group by driver,
-    # Add all of the points up
-    # Order by points DESC
+    race_results = []
+    # A driver row is the name, individual track results, and total points.
+    for driver in drivers:
+        driver_name = driver['first_name'] + ' ' + driver['last_name']
+        driver_results = RaceResult.objects.filter(driver__id=driver['id']).values_list(
+            'points', flat=True).order_by('grand_prix__race_date')
+        driver_row = [driver_name] + \
+            list(driver_results) + [driver['total_points']]
+        # TODO: Find a way to fill missing values. Vettel and Hulk subs
+        if len(driver_row) < len(headers):
+            for i in range(len(headers) - len(driver_row)):
+                driver_row.append('0')
+        race_results.append(driver_row)
+    print(race_results)
 
-    # for driver_results in all_results:
-    #     for result in driver_results:
-    #         # retrieve points value assuming they are in order
-
-    import pdb
-    pdb.set_trace()
     context = {
         'headers': headers,
-        'race_results': results,
+        'race_results': race_results,
     }
 
     return render(request, 'core/home.html', context)
