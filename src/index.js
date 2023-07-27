cb12b4e0c9e3562de402a28cb8becac144429262
Julio Uribe <uribejulioc@gmail.com>
2023-07-26 17:46:58 -0700

    Finish refactor by drying up remaining code in index.js

--- src/index.js
+++ src/index.js
@@ -5,49 +5,35 @@ import * as tableFunctions from './scripts/table-functions';
 import { StateManager } from './scripts/state-manager';
 import * as utils from './scripts/utils';
 
-let driverView;
-let driverNum;
-
-// Main
+// Initialize instance of StateManger and select elements for event handling.
 const pageManager = new StateManager();
 const filtersForm = document.querySelector(".data-filters");
 const seasonSelection = document.querySelector("#season");
 const championship = document.querySelector("#championship");
 
-const changeSeasonRefresh = e => {
+// Event Handlers.
+const tableRefresh = e => {
+  // General table refresh.
   pageManager.season = document.getElementById("season").value
   pageManager.championshipToggle(document.querySelector('#constructor').checked)
-  // When you change season don't pass in start/end date.
-  populatePage("", "");
+  populatePage();
 }
 
 const repopulatePage = e => {
-  // This can be triggered but the submit action.
+  // This is the event handler for the submit action.
   e.preventDefault();
   pageManager.season = document.getElementById("season").value
+  pageManager.championshipToggle(document.querySelector('#constructor').checked)
   pageManager.processDateRange(
     document.getElementById("start-date").value,
     document.getElementById("end-date").value,
     true
   )
-  pageManager.championshipToggle(document.querySelector('#constructor').checked)
-  // Driver num is not enough. Order of drivers changes depending on how you filter
-  // Figure out a way to pass the driver name or something and filter on that.
-  // if (driverView) {
-  //   populatePage(season, inputStartDate, inputEndDate, constructors, driverNum)
-  // } else {
-  // }
-  // perhaps save what the last submitted dates were in case we jump to driver view
-  populatePage();
-}
-
-const championshipToggle = e => {
-  pageManager.season = document.getElementById("season").value
-  pageManager.championshipToggle(document.querySelector('#constructor').checked)
   populatePage();
 }
 
 const handleDriverClick = (e, legendItem, _) => {
+  // Switches to the Driver Detail View.
   pageManager.season = document.getElementById("season").value
   pageManager.processDateRange(
     document.getElementById("start-date").value,
@@ -59,8 +45,9 @@ const handleDriverClick = (e, legendItem, _) => {
 }
 
 const backToMain = e => {
-  pageManager.clearDriverView();
+  // Resets back to Driver Championship View.
   pageManager.season = document.getElementById("season").value
+  pageManager.clearDriverView();
   pageManager.processDateRange(
     document.getElementById("start-date").value,
     document.getElementById("end-date").value
@@ -92,7 +79,6 @@ async function populatePage() {
   console.log(sortedDrivers)
   // Update start/end date dropdowns.
   utils.createStartEndDropdown(jsonData, pageManager);
-
   // Render chart depending on which options are enabled.
   const ctx = utils.handleCanvas(StateManager.currentChart);
   if (pageManager.driverView) {
@@ -114,11 +100,12 @@ async function populatePage() {
   pointsTable.appendChild(table);
 }
 
-// Main part two
-populatePage();
-utils.createSeasonSelectDropdown();
-
 // Event Listeners
 filtersForm.addEventListener("submit", repopulatePage);
-seasonSelection.addEventListener("change", changeSeasonRefresh);
-championship.addEventListener("change", championshipToggle);
+seasonSelection.addEventListener("change", tableRefresh);
+championship.addEventListener("change", tableRefresh);
+
+// Run initial populate page with defaults: season 2021, driver's championship.
+populatePage();
+// We only need to populate the season dropdown once.
+utils.createSeasonSelectDropdown();
